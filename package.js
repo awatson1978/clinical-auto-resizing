2ca2a50b1724780218a5d4d53fb83ad75e1c77c4
Abigail Watson <awatson1978@gmail.com>
2016-12-03 23:47:30 -0600

    updated meteor-platform dep

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Hooks for resizing UI elements.',
-  version: '0.3.1',
+  version: '0.3.2',
   name: 'clinical:auto-resizing',
   git: 'http://github.com/awatson1978/auto-resizing.git'
 });
@@ -8,7 +8,7 @@ Package.describe({
 Package.on_use(function (api) {
   api.versionsFrom('1.1.0.3');
 
-  api.use('meteor-platform');
+  api.use('meteor-platform@1.2.6');
   api.use('session@1.1.7');
 
   api.addFiles('lib/resizing.js', 'client');
