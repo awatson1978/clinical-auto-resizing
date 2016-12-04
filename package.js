4a4a1720e4ddd41f94ec99572678b01e42492097
Abigail Watson <awatson1978@gmail.com>
2016-12-03 20:50:28 -0600

    METEOR@1.4.2.3

--- package.js
+++ package.js
@@ -1,17 +1,13 @@
 Package.describe({
   summary: 'Hooks for resizing UI elements.',
-  version: '0.2.0',
+  version: '0.3.0',
   name: 'clinical:auto-resizing',
   git: 'http://github.com/awatson1978/auto-resizing.git'
 });
 
 Package.on_use(function (api) {
-  // we want to keep this at 1.1.0.3 as long as possible for backwards compatibility
-  // avoid upgrading .versionsFrom() unless it's a release-track wide refactor
   api.versionsFrom('1.1.0.3');
 
-  api.use('meteor-platform');
-  api.use('templating');
   api.use('session');
 
   api.addFiles('lib/resizing.js', 'client');
